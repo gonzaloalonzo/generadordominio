7deaf7e3fc6f41518ee1b49cf996232e7588088a
gonzaloalonzo <gonzaloalonzo@hotmail.com>
2020-07-29 19:30:44 -0400

    actualizacion codigo

--- gen.js
+++ gen.js
@@ -2,12 +2,29 @@ var pronoun = ['the','our'];
 var adj = ['great', 'big' ];
 var noun = ['jogger','racoon'];
 
-function init(){
-  for (let i = 0; i<adj.length; i++){
-    console.log("www."+adj[i]+noun[i]+pronoun[i]+".cl");
-    console.log("www."+pronoun[i]+adj[i]+noun[i]+".cl");
-    console.log("www."+noun[i]+pronoun[i]+adj[i]+".cl");
-    console.log("www."+pronoun[i]+noun[i]+adj[i]+".cl");
+// recorre matriz pronoun
+for (let i = 0; i < pronoun.length; i++){
+  // recorre matriz adj
+  for (let j = 0; j < adj.length; j++) {
+    //recorre matriz noun
+    for (let f = 0; f < noun.length; f++) {
+      //imprime o pinta en cosola cada elemento de las matrices
+      console.log("www."+pronoun[i]+adj[j]+noun[f]+".cl")   
+    }
+  }
+}
+
+
+
+
+
+
+/* function init(){
+  for (let index = 0; index<adj.length; index++){
+    console.log("www." + adj[index] + noun[index] + pronoun[index] + ".cl");
+    console.log("www." + pronoun[index] + adj[index] + noun[index] + ".cl");
+    console.log("www." + noun[index] + pronoun[index] + adj[index] + ".cl");
+    console.log("www." + pronoun[index] + noun[index] + adj[index] + ".cl");
   }//usar querySetector y innerhttp
 }
-init();
+init(); */
